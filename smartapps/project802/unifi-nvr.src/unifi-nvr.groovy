8ed1664a74129398e17d0f2084dad71c623020f4
Igor Matsanyuk <st@dachnik.me>
2017-05-10 18:50:45 +0000

    update

--- smartapps/project802/unifi-nvr.src/unifi-nvr.groovy
+++ smartapps/project802/unifi-nvr.src/unifi-nvr.groovy
@@ -25,67 +25,57 @@ definition(
     iconUrl: "http://project802.net/smartthings/smartapp-icons/ubiquiti_nvr.png",
     iconX2Url: "http://project802.net/smartthings/smartapp-icons/ubiquiti_nvr_2x.png",
     iconX3Url: "http://project802.net/smartthings/smartapp-icons/ubiquiti_nvr_3x.png")
-
-
 preferences {
-    input name: "nvrAddress", type: "text", title: "NVR Address", description: "NVR IP address", required: true, displayDuringSetup: true, defaultValue: "10.0.0.205"
+    input name: "nvrAddress", type: "text", title: "NVR Address", description: "NVR IP address", required: true, displayDuringSetup: true, defaultValue: "vtp.imi.vc"
     input name: "nvrPort", type: "number", title: "NVR Port", description: "NVR HTTP port", required: true, displayDuringSetup: true, defaultValue: 7080
-    input name: "username", type: "text", title: "Username", description: "Username", required: true, displayDuringSetup: true, defaultValue: "test@project802.net"
-    input name: "password", type: "text", title: "Password", description: "Password", required: true, displayDuringSetup: true, defaultValue: "unifitest"
+    input name: "username", type: "text", title: "Username", description: "Username", required: true, displayDuringSetup: true, defaultValue: "vtp.in@dachnik.me"
+    input name: "password", type: "text", title: "Password", description: "Password", required: true, displayDuringSetup: true, defaultValue: "130178231280"
+    input name: "apiKey", type: "text", title: "API Key", description: "API key", required: true, displayDuringSetup: true, defaultValue: "Z6ziFLvsMEYjpvzw"
 }
-
 /**
  * installed() - Called by ST platform
  */
 def installed() {
     log.info "UniFi NVR: installed with settings: ${settings}"
 }
-
 /**
  * updated() - Called by ST platform
  */
 def updated() {
     log.info "UniFi NVR: updated with settings: ${settings}"
-    
     nvr_initialize()
 }
-
 /**
  * nvr_initialize() - Clear state and poll the bootstrap API and the result is handled by nvr_bootstrapPollCallback
  */
 def nvr_initialize()
 {
     state.nvrName = "Unknown"
-    state.loginCookie = null;
-    state.apiKey = "";
-    
+    state.loginCookie = "";
+    state.apiKey = "${settings.apiKey}"
     state.nvrTarget = "${settings.nvrAddress}:${settings.nvrPort}"
     log.info "nvr_initialize: NVR API is located at ${state.nvrTarget}"
-
     def hubAction = new physicalgraph.device.HubAction(
-        [
-            path: "/api/2.0/login",
-            method: "POST",
-            HOST: state.nvrTarget,
-            body: "{\"email\":\"${settings.username}\", \"password\":\"${settings.password}\"}",
-            headers: [
-                "Host":"${state.nvrTarget}",
-                "Accept":"application/json",
-                "Content-Type":"application/json"
-            ]        
-        ],
-        null,
-        [
-            callback: nvr_loginCallback 
-        ]
-    );
+    [
+        path: "/api/2.0/login HTTP/1.1\r\n",
+        method: "POST",
+        protocol: physicalgraph.device.Protocol.LAN,
+        HOST: state.nvrTarget,
+        body: "{\"email\":\"${settings.username}\", \"password\":\"${settings.password}\"}",
+        headers: [
+            "Host":"${state.nvrTarget}",
+            "Accept":"application/json",
+            "Content-Type":"application/json"
+        ]        
+    ],
+    null,
+    [
+        callback: nvr_loginCallback 
+    ]
+);
 
-    sendHubCommand( hubAction );
+sendHubCommand( hubAction );
 }
-
-/**
- * nvr_loginCallback() - Callback from hubAction that sends the login API request
- */
 def nvr_loginCallback( physicalgraph.device.HubResponse hubResponse )
 {
     if( hubResponse.status != 200 )
@@ -93,16 +83,7 @@ def nvr_loginCallback( physicalgraph.device.HubResponse hubResponse )
         log.error "nvr_loginCallback: unable to login.  Please check IP, username and password.  Status ${hubResponse.status}.";
         return;
     }
-    
     String setCookieHeader = hubResponse?.headers['set-cookie'];
-    
-    if( !setCookieHeader )
-    {
-        log.error "nvr_loginCallback: no headers found for login token.  Please check IP, username and password.";
-        return;
-    }
-    
-    // JSESSIONID_AV is the login cookie we need to use for other API calls
     def cookies = setCookieHeader.split(";").inject([:]) { cookies, item ->
         def nameAndValue = item.split("=");
         if( nameAndValue[0] == "JSESSIONID_AV" )
@@ -110,7 +91,6 @@ def nvr_loginCallback( physicalgraph.device.HubResponse hubResponse )
             state.loginCookie = nameAndValue[1];
         }
     }
-    
     if( !state.loginCookie )
     {
         log.error "nvr_loginCallback: unable to login.  Please check IP, username and password.";
@@ -121,15 +101,12 @@ def nvr_loginCallback( physicalgraph.device.HubResponse hubResponse )
     {
         log.info "nvr_loginCallback: login successful!";
     }
-    
-    // If there is no API key or its off, the cameras won't work.
-    // [todo] add API key validation in SmartApp?
     state.apiKey = hubResponse.json?.data?.apiKey[0];
-    
     def hubAction = new physicalgraph.device.HubAction(
         [
-            path: "/api/2.0/bootstrap",
+            path: "/api/2.0/bootstrap HTTP/1.1\r\n",
             method: "GET",
+            protocol: physicalgraph.device.Protocol.LAN,
             HOST: state.nvrTarget,
             headers: [ 
                 "Host":"${state.nvrTarget}", 
@@ -143,74 +120,66 @@ def nvr_loginCallback( physicalgraph.device.HubResponse hubResponse )
             callback: nvr_bootstrapPollCallback 
         ]
     );
-
     sendHubCommand( hubAction );
 }
-
 /**
  * nvr_bootstrapPollCallback() - Callback from HubAction with result from GET
  */
 def nvr_bootstrapPollCallback( physicalgraph.device.HubResponse hubResponse )
 {
     def data = hubResponse.json?.data
-    
     if( !data || !data.isLoggedIn )
     {
     	log.error "nvr_bootstrapPollCallback: unable to get data from NVR!"
         return
     }
-    
     if( data.isLoggedIn[0] != true )
     {
     	log.error "nvr_bootstrapPollCallback: unable to log in!  Please check API key."
         return
     }
-    
     state.nvrName = data.servers[0].name[0]
     log.info "nvr_bootstrapPollCallback: response from ${state.nvrName}"
-    
-    def camerasProcessed = 0
-    
-    data.cameras[0].each { camera ->
-        def dni = "${camera.mac}"
-        def child = getChildDevice( dni )
-        
-        ++camerasProcessed
-        
-        if( child )
-        {
-            log.info "nvr_bootstrapPollCallback: already have child ${dni}"
-            child.updated()
-        }
-        else
-        {
-            def metaData = [   "label" : camera.name + " (" + camera.model + ")",
-                               "data": [
-                                   "uuid" : camera.uuid,
-                                   "name" : camera.name,
-                                   // 1st generation: doesn't enumerate the 'camera.platform', uses camera.uuid for API calls
-                                   // 2nd generation: camera.platform = "GEN2", uses camera._id for API calls
-                                   // 3rd generation: camera.platform = "GEN3L", uses camera._id for API calls
-                                   "id" : camera.platform ? camera._id : camera.uuid
-                               ]
-                           ]
-                           
-            log.info "nvr_bootstrapPollCallback: adding child ${dni} ${metaData}"
-            
-            try
-            {
-                addChildDevice( "project802", "UniFi NVR Camera", dni, location.hubs[0].id, metaData )
-            }
-            catch( exception )
-            {
-                log.error "nvr_bootstrapPollCallback: adding child ${dni} failed (child probably already exists), continuing..."
-            }
-        }
-    }
-    
-    log.info "nvr_bootstrapPollCallback: processed ${camerasProcessed} cameras"
+    if( data.cameras[0].size < 1 )
+{
+	log.warn "nvr_bootstrapPollCallback: no cameras found!"
+	return
 }
 
+log.info "nvr_bootstrapPollCallback: found ${data.cameras[0].size} camera(s)"
+
+data.cameras[0].each { camera ->
+    def dni = "${camera.mac}"
+    def child = getChildDevice( dni )
+    
+    if( child )
+    {
+        log.info "nvr_bootstrapPollCallback: already have child ${dni}"
+        child.updated()
+    }
+    else
+    {
+        def metaData = [   "label" : camera.name + " (" + camera.model + ")",
+                           "data": [
+                               "uuid" : camera.uuid,
+                               "name" : camera.name,
+                               "id" : camera._id
+                           ]
+                       ]
+                       
+        log.info "nvr_bootstrapPollCallback: adding child ${dni} ${metaData}"
+        
+        try
+        {
+            addChildDevice( "project802", "UniFi NVR Camera", dni, location.hubs[0].id, metaData )
+        }
+        catch( exception )
+        {
+            log.error "nvr_bootstrapPollCallback: adding child ${dni} failed (child probably already exists), continuing..."
+        }
+    }
+}
+}
 /**
  * _getApiKey() - Here for the purpose of children
  */
